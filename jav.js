c2c442c71ab4c89c2c1b609707bc0243f52b58be
PGMSE <76865140+PGMSE@users.noreply.github.com>
2021-01-11 01:21:04 +0530

    My First Website
    MyFirstWebsite

---
+++ jav.js
@@ -0,0 +1,7 @@
+
+
+function myfun()
+{
+    document.getElementById('lan').style.display='grid';
+    document.getElementById('abc').style.display='none';
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Hi Guyz this is Going to be my first Combined site for All years of B.tech in Material Science So Guyz PLEASE add a like to my site"
+
+function myfun()
+{
+    document.getElementsById("lan").style.display="grid";
+}
