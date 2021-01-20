fcdc353946654241f1a5200d75840297454cc4b9
PGMSE <76865140+PGMSE@users.noreply.github.com>
2021-01-20 15:17:39 +0530

    Updation 1
    Second Yr Page Created

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-alert("Hi Guyz this is Going to be my first Combined site for All years of B.tech in Material Science So Guyz PLEASE add a like to my site"
+
 
 function myfun()
 {
---
+++ sec.js
@@ -0,0 +1,38 @@
+
+function drop()
+{
+    document.getElementById("dp").style.display="block";
+}
+function drip1()
+{
+    document.getElementById("try").style.display="none";
+    document.getElementById("dp").style.display="none";
+    document.getElementById("ht").style.display="grid";
+}
+function drip2()
+{
+    document.getElementById("try").style.display="none";
+    document.getElementById("dp").style.display="none";
+    document.getElementById("pst").style.display="grid";
+}
+function drip3()
+{
+    document.getElementById("try").style.display="none";
+    document.getElementById("dp").style.display="none";
+    document.getElementById("math").style.display="grid";
+}
+function drip4()
+{
+    document.getElementById("try").style.display="none";
+    document.getElementById("dp").style.display="none";
+    document.getElementById("cm").style.display="grid";
+}
+function drip5()
+{
+    document.getElementById("try").style.display="none";
+    document.getElementById("dp").style.display="none";
+    document.getElementById("mpt").style.display="grid";
+}
+
+
+
